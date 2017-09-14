7c35e19a40dbe8c449d3a3f8a9db501d46dbd296
KimYuRa1 <31638973+KimYuRa1@users.noreply.github.com>
2017-09-14 15:24:32 +0900

    5th

---
+++ Dog.java
@@ -0,0 +1,16 @@
+
+public class Dog extends Animal {
+
+	public Dog() {
+		super();
+		System.out.println("A new dog has been created!");
+	}
+	
+	public void sleep() {
+		System.out.println("A dog sleeps...");
+	}
+	
+	public void eat() {
+		System.out.println("A dog eats...");
+	}
+}
